b94f4506a97936cfa42c9ae6d737463525324750
Sp1dz <spidepog@gmail.com>
2022-03-29 10:57:07 +0300

    Implemented profile token card creation

--- Program.cs
+++ Program.cs
@@ -9,6 +9,23 @@ static class Program
     [STAThread]
     static void Main()
     {   
+        string userName = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+        int i = userName.LastIndexOf('\\');
+        userName = userName.Substring(i + 1, userName.Length - i - 1);
+
+        string path = @"gamedata/profile/" + userName + "_card";
+        if (!File.Exists(path)) {
+            System.Security.Cryptography.RNGCryptoServiceProvider rngCsp = new();
+            System.IO.Directory.CreateDirectory(@"gamedata/profile/");
+            using (FileStream fs = File.Create(path))
+            {
+
+                byte[] info = new byte[500];
+                rngCsp.GetBytes(info);
+                fs.Write(info, 0, info.Length);
+            }
+        }
+
         ApplicationConfiguration.Initialize();
         Form1 mainForm = new Form1();
         MenuScene mainMenu = new();
